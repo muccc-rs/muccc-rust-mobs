13d889c40ec6b5363f25a8f644f4c914625fa83d
Maximilian Berger <max@geile.software>
2024-03-15 23:59:42 +0100

    sunny balls ☀️☀️🏈⚾️

---
+++ raytrace/src/colour.rs
@@ -0,0 +1,52 @@
+use nalgebra as na;
+
+/// Blackbody radiation from a temperature in Kelvin
+pub fn colour_from_kelvin(dungerees_kelvin: f32) -> na::Vector3<f32> {
+    let dungerees_kelvin = dungerees_kelvin / 100.0;
+    let r = if dungerees_kelvin <= 66.0 {
+        255.0
+    } else {
+        let r = 329.698727446 * (dungerees_kelvin - 60.0).powf(-0.1332047592);
+        if r > 255.0 {
+            255.0
+        } else if r < 0.0 {
+            0.0
+        } else {
+            r
+        }
+    };
+    let g = if dungerees_kelvin <= 66.0 {
+        let g = 99.4708025861 * dungerees_kelvin.ln() - 161.1195681661;
+        if g > 255.0 {
+            255.0
+        } else if g < 0.0 {
+            0.0
+        } else {
+            g
+        }
+    } else {
+        let g = 288.1221695283 * (dungerees_kelvin - 60.0).powf(-0.0755148492);
+        if g > 255.0 {
+            255.0
+        } else if g < 0.0 {
+            0.0
+        } else {
+            g
+        }
+    };
+    let b = if dungerees_kelvin >= 66.0 {
+        255.0
+    } else if dungerees_kelvin <= 19.0 {
+        0.0
+    } else {
+        let b = 138.5177312231 * (dungerees_kelvin - 10.0).ln() - 305.0447927307;
+        if b > 255.0 {
+            255.0
+        } else if b < 0.0 {
+            0.0
+        } else {
+            b
+        }
+    };
+    na::Vector3::new(r, g, b)
+}
--- raytrace/src/main.rs
+++ raytrace/src/main.rs
@@ -1,9 +1,13 @@
+use std::collections::btree_set::Intersection;
+
+use colour::colour_from_kelvin;
+use na::OPoint;
 use nalgebra as na;
 
+mod colour;
 mod ppm;
 
 struct Skybox {
-    z: f32,
     ground_color: na::Vector3<f32>,
     sky_color: na::Vector3<f32>,
 }
@@ -14,13 +18,27 @@ struct Sphere {
     color: na::Vector3<f32>,
 }
 
+struct Scene {
+    objects: Vec<Object>,
+    light: PointLight,
+}
+
+struct PointLight {
+    position: na::Point3<f32>,
+    color: na::Vector3<f32>,
+}
+
 enum Object {
     Skybox(Skybox),
     Sphere(Sphere),
 }
 
+const SURFACE_TEMP_SUN_KELVIN: f32 = 5778.0; // pretty hot
+const SURFACE_TEMP_EARTH_KELVIN: f32 = 288.0; // pretty nice
+const SURFACE_TEMP_ICE_KELVIN: f32 = 273.0; // pretty cold
+
 fn main() {
-    let camera_position = na::Point3::new(10.0, 10.0, 10.0);
+    let camera_position = na::Point3::new(0., 0., 100.0);
     let view_target = na::Point3::new(0.0, 0.0, 0.0);
     let up = na::Vector3::new(0.0, 1.0, 0.0);
 
@@ -29,29 +47,65 @@ fn main() {
     // let aspect_ratio = 1.0;
     // let perspective =
     //     na::Matrix4::new_perspective(aspect_ratio, std::f32::consts::FRAC_PI_4, 0.001, 100.0);
+    let sun = PointLight {
+        position: na::Point3::new(-10., 50., 0.),
+        color: colour_from_kelvin(SURFACE_TEMP_SUN_KELVIN),
+    };
 
-    let scene = vec![Object::Skybox(Skybox {
-        z: 0.0,
-        ground_color: na::Vector3::new(0.0, 255.0, 0.0),
-        sky_color: na::Vector3::new(0.0, 0.0, 255.0),
-    })];
+    let mut scene = Scene {
+        objects: vec![
+            Object::Skybox(Skybox {
+                ground_color: na::Vector3::new(0.0, 255.0, 0.0),
+                sky_color: na::Vector3::new(0.0, 0.0, 255.0),
+            }),
+            Object::Sphere(Sphere {
+                center: na::Point3::new(0., 0., 0.),
+                radius: 10.,
+                color: na::Vector3::new(255., 0., 0.),
+            }),
+            Object::Sphere(Sphere {
+                center: na::Point3::new(12., 0., 0.),
+                radius: 5.,
+                color: na::Vector3::new(255., 0., 255.),
+            }),
+            // lmao sun
+            Object::Sphere(Sphere {
+                center: sun.position, // - na::Vector3::new(0., 5., 0.),
+                radius: 10.,
+                color: sun.color,
+            }),
+        ],
+        light: sun,
+    };
+    for i in 0..30 {
+        scene.objects.push(Object::Sphere(Sphere {
+            center: na::Point3::new(i as f32, 0., 0.),
+            radius: 1.,
+            color: na::Vector3::new(255., 0., 0.),
+        }));
+        scene.objects.push(Object::Sphere(Sphere {
+            center: na::Point3::new(0., i as f32, 0.),
+            radius: 1.,
+            color: na::Vector3::new(0., 255., 0.),
+        }));
+        scene.objects.push(Object::Sphere(Sphere {
+            center: na::Point3::new(0., 0., i as f32),
+            radius: 1.,
+            color: na::Vector3::new(0., 0., 255.),
+        }));
+    }
 
     let xres = 1024;
     let yres = 1024;
 
     let mut pixels = vec![[0, 0, 0]; xres * yres];
 
-    println!(
-        "{}\t{}\t{}",
-        camera_position.x, camera_position.y, camera_position.z
-    );
-
     for ix in 0..xres {
         for iy in 0..yres {
             let ray_target_viewspace = na::Point3::new(
                 ix as f32 / (xres - 1) as f32 * 2.0 - 1.0,
-                (iy as f32 / (yres - 1) as f32 * 2.0 - 1.0) * -1.0,
-                1.0,
+                (iy as f32 / (yres - 1) as f32 * 2.0 - 1.0) * -1.,
+                -1.0,
             );
 
             let ray_target_worldspace = view
@@ -59,24 +113,27 @@ fn main() {
                 .unwrap()
                 .transform_point(&ray_target_viewspace);
 
-            println!(
-                "{}\t{}\t{}",
-                ray_target_worldspace.x, ray_target_worldspace.y, ray_target_worldspace.z
-            );
-
             let ray_vector = ray_target_worldspace - camera_position;
-            let color_at_pixel = scene
-                .iter()
-                .map(|obj| intersect(obj, camera_position, ray_vector))
-                .min_by(|(_, d1), (_, d2)| d1.partial_cmp(d2).unwrap())
-                .map(|(color, _)| color)
-                .unwrap();
+            if ix == 0 && iy == 0 {
+                dbg!(ray_vector);
+            }
+            let intersection = trace_ray(&scene, camera_position, ray_vector);
+
+            // Phake Phong Lighting
+            let intersection_point = camera_position + ray_vector * intersection.distance;
+            let light_to_telekom = -(intersection_point - scene.light.position).normalize();
+            let mut color_at_pixel =
+                intersection.color * light_to_telekom.dot(&intersection.normal).max(0.);
+            color_at_pixel += intersection.color * 0.5;
 
             pixels[ix + iy * xres] = [
                 color_at_pixel.x as u8,
                 color_at_pixel.y as u8,
                 color_at_pixel.z as u8,
             ];
+            if iy == 0 {
+                pixels[ix + iy * xres] = [255, 0, 0];
+            }
         }
     }
 
@@ -89,17 +146,64 @@ fn main() {
     .unwrap();
 }
 
-fn intersect(obj: &Object, cam: na::Point3<f32>, ray: na::Vector3<f32>) -> (na::Vector3<f32>, f32) {
+fn trace_ray(
+    scene: &Scene,
+    camera_position: na::Point3<f32>,
+    ray_vector: na::Vector3<f32>,
+) -> IntersectionResult {
+    scene
+        .objects
+        .iter()
+        .filter_map(|obj| intersect(obj, camera_position, ray_vector))
+        .min_by(|r1, r2| r1.distance.partial_cmp(&r2.distance).unwrap())
+        .unwrap()
+}
+
+struct IntersectionResult {
+    color: na::Vector3<f32>,
+    distance: f32,
+    normal: na::Vector3<f32>,
+}
+
+fn intersect(
+    obj: &Object,
+    cam: na::Point3<f32>,
+    ray: na::Vector3<f32>,
+) -> Option<IntersectionResult> {
     match obj {
-        Object::Skybox(skybox) => {
-            if ray.y < skybox.z {
-                (skybox.ground_color, f32::INFINITY)
+        Object::Skybox(skybox) => Some(IntersectionResult {
+            color: if ray.y < 0. {
+                skybox.ground_color
             } else {
-                (skybox.sky_color, f32::INFINITY)
-            }
-        }
+                skybox.sky_color
+            },
+            distance: f32::INFINITY,
+            normal: -ray.normalize(),
+        }),
         Object::Sphere(s) => {
-            unimplemented!();
+            // original code do not steal
+            //vec3 oc = r.origin() - center;
+            //float a = dot(r.direction(), r.direction());
+            //float b = 2.0 * dot(oc, r.direction());
+            //float c = dot(oc,oc) - radius*radius;
+            //float discriminant = b*b - 4*a*c;
+            //return (discriminant>0);
+
+            let oc = cam - s.center;
+            let a = ray.dot(&ray);
+            let b = 2. * oc.dot(&ray);
+            let c = oc.dot(&oc) - s.radius * s.radius;
+            let discriminant = b * b - 4. * a * c;
+            if discriminant > 0. {
+                let dist = (-b - discriminant.sqrt()) / (2.0 * a);
+                Some(IntersectionResult {
+                    color: s.color,
+                    distance: dist,
+                    normal: (cam + dist * ray - s.center).normalize(),
+                })
+            } else {
+                None
+            }
         }
     }
 }
