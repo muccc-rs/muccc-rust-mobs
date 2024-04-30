7db2ac52c03e31282cfb2ee39efc614fd5655cba
Rahix <rahix@rahix.de>
2024-04-30 18:56:30 +0200

    Start adding a paywall

---
+++ paywall/src/main.rs
@@ -0,0 +1,21 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/{id}/{name}/index.html")]
+async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
+    format!("Hello {}! id:{}", name, id)
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    format!("Hello {}! id:{}", name, id)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new()
+        .service(index))
+        .route("/{filename:.*}")
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
+}
