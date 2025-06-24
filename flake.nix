6279ffd96c68bd0bb2d758ca659fb998f84dfd6f
Franz Pletz <fpletz@fnordicwalking.de>
2025-06-24 23:27:30 +0200

    flake: remove pre-commit-hooks

--- flake.nix
+++ flake.nix
@@ -6,17 +6,6 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        nixpkgs-stable.follows = "nixpkgs";
-      };
-    };
   };
 
   outputs =
@@ -27,11 +16,6 @@
         "aarch64-linux"
         "riscv64-linux"
         "aarch64-darwin"
-      ];
-
-      imports = [
-        inputs.treefmt-nix.flakeModule
-        inputs.pre-commit-hooks.flakeModule
       ];
 
       perSystem =
@@ -66,7 +50,6 @@
                       cargo-tarpaulin
                       cargo-machete
                       llvmPackages.lld
-                      config.treefmt.build.wrapper
                       openssl
                       zlib
                       rust-analyzer
@@ -87,36 +70,8 @@
                   CARGO_TERM_COLOR = "always";
                   shellHook = ''
                     export XDG_DATA_DIRS=$GSETTINGS_SCHEMAS_PATH
-                    ${config.pre-commit.installationScript}
                   '';
                 };
-          };
-
-          pre-commit = {
-            check.enable = true;
-            settings.hooks.treefmt = {
-              enable = true;
-            };
-          };
-          treefmt = {
-            projectRootFile = "flake.lock";
-
-            settings = {
-              formatter = {
-                nix = {
-                  command = pkgs.nixfmt-rfc-style;
-                  includes = [ "*.nix" ];
-                };
-                rustfmt = {
-                  command = pkgs.rustfmt;
-                  options = [
-                    "--edition"
-                    "2024"
-                  ];
-                  includes = [ "*.rs" ];
-                };
-              };
-            };
           };
         };
     };
